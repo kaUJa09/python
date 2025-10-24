94646602cea068cb6f13837e533cebb29c1f29a2
abbaspazlidinov7@gmail.com <abbaspazlidinov7@gmail.com>
2025-10-24 21:07:21 +0600

    friday commit

---
+++ secondLesson.py
@@ -0,0 +1,4 @@
+food = 'pizza'
+drink = 'tea'
+print('my favorite food is', food, 'and my favorite drink is', drink)
+print(f'my favorite food is {food} and my favorite drink is {drink}')
---
+++ thirdLesson.py
@@ -0,0 +1,19 @@
+# products = list('milk', 'bread', 'eggs', 'apples', 'bananas', 'cheese', 'meat', 'ketchup', 'mayo', 'salt')
+# products = ['milk', 'bread', 'eggs', 'apples', 'bananas', 'cheese', 'meat', 'ketchup', 'mayo', 'salt']
+# print('we have these products')
+# customer = input('i want')
+# price = int(1000)
+# print('price is', price, 'som')
+num1 = 2
+num2 = 3
+num3 = 1
+print(2 * (3 - 1))
+print((5-1)*(5+1))
+print(0.3*(4-1))
+print((91-1)/(2+1))
+print(5*4/3 ** 0.5)
+print((1)/(3)+5*(0.2-0.001)/(91))
+
+print((0.16*(3.2-(3/40))+2*(3/11)*4.125/3*(3/4))/5*(1/6)*0.3-0.3*4.5+(1/3)*0.3)
+
+
