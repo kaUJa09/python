dd81ed3a66bfa6d33f5fd14a04bf032367948554
abbaspazlidinov7@gmail.com <abbaspazlidinov7@gmail.com>
2025-10-29 20:59:10 +0600

    29october

---
+++ sixthLesson.py
@@ -0,0 +1,108 @@
+# word = 'people'
+# text = 'Hello, my name is Sam'
+# list1 = [2, 4, 64, 6, 'name']
+
+# search = 'name'
+# if search in text:
+#     print(f'{search} is in text')
+# else:
+#     print(f'{search} is not in the text')
+
+# if search in list1:
+#     print(f'{search} is in list1 {list1}')
+# else:
+#     print(f'{search} is not in the list1 {list1}')
+
+# num1 = int(input('enter a num:'))
+# if num1  % 2 == 0:
+#     print('четное')
+# else:
+#     print('не четное')
+
+
+# A = int(input("Введите число: "))
+# num = A > 0
+# print(num)
+
+
+# A = int(input("Введите число:"))
+# num1 = A % 2 != 0
+# print(num1)
+
+
+# A = int(input("Введите число:"))
+# num2 = A % 2 == 0
+# print(num2)
+
+
+# A = int(input('Введите число:'))
+# B = int(input('Введите число:'))
+# num3 =  A > 2 and B <= 3
+# print(num3)
+
+
+# A = int(input('Введите число:'))
+# B = int(input('Введите число:'))
+# num4 =  A >= 0 and B > -2
+# print(num4)
+
+
+# A = int(input('Введите число:'))
+# B = int(input('Введите число:'))
+# C = int(input('Введите число:'))
+# num5 = A < B < C
+# print(num5)
+
+
+# A = int(input('Введите число:'))
+# B = int(input('Введите число:'))
+# C = int(input('Введите число:'))
+# num6 = (A < B < C) or (C < B < A)
+# print(num6)
+
+
+# word = 'milk'
+# list1 = ['apple', 'banana', 'milk']
+# if 'milk' in list1:
+#     new_word = word.replace('k', 'che')
+#     print(new_word)
+
+# text3 = 'lesson5'
+# new_text = text3.replace()
+# print(new_text)
+
+
+# name = ' aBBAS khkj kjhkj'
+# # print(len(name))
+# name2 = name.split()
+# print(name2)
+# print(len(name2))
+
+
+# a = 32
+# b = 3.5
+# x = True
+
+# a = str(a)
+# b = str(b)
+# x = str(x)
+
+# print(a, type(a))
+# print(b, type(b))
+# print(x, type(x))
+
+
+# str_ = input('enter')
+# print(str_)
+
+
+name = input('Write your name')
+surname = input('Write your surname')
+print(name)
+print(surname)
+
+
+
+
+
+
